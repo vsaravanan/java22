350269c249e9671fbfada75bc7bbee69177d3525
Viswanathan Saravanan <saravanan.resume@gmail.com>
2024-06-18 18:36:03 +0800

    WaitNotifyTest

--- src/main/java/com/saravanjs/java22/console/multithreading/WaitNotifyTest.java
+++ src/main/java/com/saravanjs/java22/console/multithreading/WaitNotifyTest.java
@@ -1,5 +1,7 @@
 package com.saravanjs.java22.console.multithreading;
 
+import java.util.Scanner;
+
 /**
  * @author Sarav on 18 Jun 2024
  * @project govtech
@@ -7,9 +9,7 @@ package com.saravanjs.java22.console.multithreading;
  * @class WaitNotifyTest
  *
  * https://raw.githubusercontent.com/vsaravanan/java22/master/src/main/java/com/saravanjs/java22/console/multithreading/WaitNotifyTest.java
-
-    https://www.digitalocean.com/community/tutorials/java-thread-wait-notify-and-notifyall-example
-
+ * https://www.digitalocean.com/community/tutorials/java-thread-wait-notify-and-notifyall-example
  */
 
 class Message {
@@ -30,6 +30,7 @@ class Message {
 }
 
 class Waiter implements Runnable{
+    private static int i = 0;
 
     private Message msg;
 
@@ -39,15 +40,15 @@ class Waiter implements Runnable{
 
     @Override
     public void run() {
-        String name = Thread.currentThread().getName();
+        String name = Thread.currentThread().getName() + ++i;
         synchronized (msg) {
             try{
-                System.out.println(name+" waiting to get notified at time:"+System.currentTimeMillis());
+                System.out.println(name+" waiting to get notified ");
                 msg.wait();
             }catch(InterruptedException e){
                 e.printStackTrace();
             }
-            System.out.println(name+" waiter thread got notified at time:"+System.currentTimeMillis());
+            System.out.println(name+" waiter thread got notified ");
             //process the message now
             System.out.println(name+" processed: "+msg.getMsg());
         }
@@ -69,10 +70,11 @@ class Notifier implements Runnable {
         System.out.println(name+" started");
         try {
             Thread.sleep(1000);
+            System.out.println("Resumed!");
             synchronized (msg) {
                 msg.setMsg(name+" Notifier work done");
-//                msg.notify();
-                msg.notifyAll();
+                msg.notify();
+//                msg.notifyAll();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -82,19 +84,41 @@ class Notifier implements Runnable {
 
 }
 
-public class WaitNotifyTest {
 
+public class WaitNotifyTest {
     public static void main(String[] args) {
         Message msg = new Message("process it");
-        Waiter waiter = new Waiter(msg);
-        new Thread(waiter,"waiter").start();
 
-        Waiter waiter1 = new Waiter(msg);
-        new Thread(waiter1, "waiter1").start();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter action (w=Wait, n=Notify, q=Quit):");
+            String input = scanner.nextLine().trim().toLowerCase();
 
-        Notifier notifier = new Notifier(msg);
-        new Thread(notifier, "notifier").start();
-        System.out.println("All the threads are started");
+            switch (input) {
+                case "w":
+                    System.out.println("Waiting...");
+                    Waiter waiter = new Waiter(msg);
+                    Thread waiterThread = new Thread(waiter, "waiter");
+                    waiterThread.start();
+                    break;
+                case "n":
+                    System.out.println("Notifying...");
+                    Notifier notifier = new Notifier(msg);
+                    Thread notifierThread = new Thread(notifier, "notifier");
+                    notifierThread.start();
+
+//                    synchronized (msg) {
+//                        msg.notify();
+//                    }
+                    break;
+                case "q":
+                    System.out.println("Quitting...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
+        }
     }
-
 }
